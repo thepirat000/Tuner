0cab7b3c2c7b257f5a57095d2d1a8327d7cde5a6
Federico Colombo <thepirat000@hotmail.com>
2021-05-10 01:58:47 -0500

    initial

--- docs/tuner.js
+++ docs/tuner.js
@@ -13,7 +13,7 @@ async function Scan() {
 	let id = device.id;
 	alert(name + " " + id);
 	const server = await device.gatt.connect();
-	const service = await server.getPrimaryService('getPrimaryService');
+	const service = await server.getPrimaryService('fe000000-fede-fede-0000-000000000000');
 	let characteristicCmd = await service.getCharacteristics('ca000000-fede-fede-0000-000000000001')[0];
 	let valueFreq = characteristicCmd.readValue();
 	alert("F: " + valueFreq);
