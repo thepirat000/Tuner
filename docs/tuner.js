bfa4911f7b4f886e283585cffec84a86b53c0ee2
Federico Colombo <thepirat000@hotmail.com>
2021-05-10 01:47:38 -0500

    initial

--- docs/tuner.js
+++ docs/tuner.js
@@ -9,13 +9,16 @@ $(document).ready(function () {
 async function Scan() {
 	const device = await navigator.bluetooth.requestDevice({
         filters: [{namePrefix: ['Tuner by ThePirat']}]});
-		
+	let name = device.name;
+	let id = device.id;
+	alert(name + " " + id);
 	const server = await device.gatt.connect();
 	const services = await server.getPrimaryServices();
 	const service = await services[0];
 	let characteristicCmd = await service.getCharacteristic('ca000000-fede-fede-0000-000000000001');
 	let valueFreq = characteristicCmd.readValue();
-	
+	alert("F: " + valueFreq.getString());
 	let characteristicDuties = await service.getCharacteristic('ca000000-fede-fede-0000-000000000002');
 	let valueDuties = characteristicCmd.readValue();
+	alert("D: " + valueDuties.getString());
 }
