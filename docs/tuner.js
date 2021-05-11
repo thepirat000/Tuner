cbc7f43c7b24dcf6a8726179ed30d1527284248d
Federico Colombo <thepirat000@hotmail.com>
2021-05-10 22:58:31 -0500

    initial

--- docs/tuner.js
+++ docs/tuner.js
@@ -6,6 +6,9 @@ $(document).ready(function () {
 
 });
 
+var characteristicCmd;
+var characteristicDuties;
+
 async function Scan() {
 	const device = await navigator.bluetooth.requestDevice({
         filters: [{
@@ -18,9 +21,18 @@ async function Scan() {
 
 	const server = await device.gatt.connect();
 	const service = await server.getPrimaryService('fe000000-fede-fede-0000-000000000000');
-	let characteristicCmd = await service.getCharacteristics('ca000000-fede-fede-0000-000000000001');
-	let valueFreq =	new TextDecoder().decode(await characteristicCmd[0].readValue());
-	let characteristicDuties = await service.getCharacteristics('ca000000-fede-fede-0000-000000000002');
-	let valueDuties = new TextDecoder().decode(await characteristicDuties[0].readValue());
-	alert("D: " + valueDuties + '\nF: ' + valueFreq);
+	characteristicCmd = (await service.getCharacteristics('ca000000-fede-fede-0000-000000000001'))[0];
+	characteristicDuties = (await service.getCharacteristics('ca000000-fede-fede-0000-000000000002'))[0];
+	alert("D: " + GetDuties() + '\nF: ' + GetFreqs());
+	alert("D: " + GetDuties() + '\nF: ' + GetFreqs());
+}
+
+async function GetFreqs() {
+	let valueFreq =	new TextDecoder().decode(await characteristicCmd.readValue());
+	return valueFreq;
+}
+
+async function GetDuties() {
+	let valueDuties = new TextDecoder().decode(await characteristicDuties.readValue());
+	return valueDuties;
 }
